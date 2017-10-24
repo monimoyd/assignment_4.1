5b36b3b790331e67c3f1f2961708dbc157c6de5b
monimoyd <monimoyd@gmail.com>
2017-10-24 11:00:34 +0530

    Add two numbers

---
+++ acad.java
@@ -0,0 +1,9 @@
+public class acad {
+   public static void main(String [] args) {
+       int num1 =  45;
+       int num2 = 33;
+       int sum = num1 + num2;
+       System.out.println("Sum of two numbers num1=" + num1 + ", num2=" + num2 + " is: " + sum);
+   }
+}
+      
